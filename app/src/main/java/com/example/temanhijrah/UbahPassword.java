4a0ca7bab4247a98bfd609b022fdfcad74497552
diandika <mohammaddicky.andika@gmail.com>
2019-05-23 13:55:42 +0700

    add layout in setting

---
+++ app/src/main/java/com/example/temanhijrah/UbahPassword.java
@@ -0,0 +1,13 @@
+package com.example.temanhijrah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UbahPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_password);
+    }
+}
