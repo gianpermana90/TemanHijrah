b0814b13e3d5b9781225708cda2102f8b6570021
diandika <mohammaddicky.andika@gmail.com>
2019-07-19 17:19:46 +0700

    fix bug on dzikir spinner

--- app/src/main/java/com/example/temanhijrah/DzikirCounter.java
+++ app/src/main/java/com/example/temanhijrah/DzikirCounter.java
@@ -3,12 +3,14 @@ package com.example.temanhijrah;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class DzikirCounter extends AppCompatActivity {
+public class DzikirCounter extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private int count = 0;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +33,11 @@ public class DzikirCounter extends AppCompatActivity {
     }
 
     public void addSpinner(){
-        Spinner spinner = findViewById(R.id.dzikir_list);
+        spinner = findViewById(R.id.dzikir_list);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dzikir_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void count(View view) {
@@ -45,5 +48,44 @@ public class DzikirCounter extends AppCompatActivity {
     public void resetCount(View view) {
         this.count = 0;
         putValue();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView lafal, arti;
+        switch (position) {
+            case 0:
+                lafal = (TextView) findViewById(R.id.arabic_text);
+                lafal.setText(R.string.lafal_tasbih);
+                arti = (TextView) findViewById(R.id.arti_dzikir);
+                arti.setText(R.string.arti_tasbih);
+                break;
+            case 1:
+                lafal = (TextView) findViewById(R.id.arabic_text);
+                lafal.setText(R.string.lafal_hamdalah);
+                arti = (TextView) findViewById(R.id.arti_dzikir);
+                arti.setText(R.string.arti_hamdalah);
+                break;
+            case 2:
+                lafal = (TextView) findViewById(R.id.arabic_text);
+                lafal.setText(R.string.lafal_takbir);
+                arti = (TextView) findViewById(R.id.arti_dzikir);
+                arti.setText(R.string.arti_takbir);
+                break;
+            case 3:
+                lafal = (TextView) findViewById(R.id.arabic_text);
+                lafal.setText(R.string.lafal_tahlil);
+                arti = (TextView) findViewById(R.id.arti_dzikir);
+                arti.setText(R.string.arti_tahlil);
+                break;
+            default:
+                break;
+        }
+        resetCount(view);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
